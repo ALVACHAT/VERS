d5908f5fdbd94857b56a31ffbf560d8f6777940f
ALVA AI <80362720+ALVACHAT@users.noreply.github.com>
2025-09-19 22:14:35 +0200

    Update VERS119.py
    scheduler przestawiony z 5m na 1m

--- VERS119.py
+++ VERS119.py
@@ -341,7 +341,7 @@ def main():
     dispatcher.add_handler(CommandHandler("status", status_command))
 
     scheduler = BackgroundScheduler()
-    scheduler.add_job(check_trades, 'interval', minutes=5, timezone=pytz.timezone('Europe/Warsaw'))
+    scheduler.add_job(check_trades, 'interval', minutes=1, timezone=pytz.timezone('Europe/Warsaw'))
     scheduler.start()
 
     check_trades()
